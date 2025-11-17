71fe070d9b2b00dfcdfac36121ee5db206e1b860
niv karasenty <niv.karas3nty@gmail.com>
2025-11-17 12:35:32 +0200

    redo 3 correction of conventions

--- max.c
+++ max.c
@@ -14,4 +14,5 @@ int main() {
     // We multiply the bigger value by one and the smaller one by 0, that is how we get the bigger value every time
     // I have now included the option that x and y are equal, in that case both the x>y and x<y will be 0 and we will add either of the values for the solution to be the value they are equal to
     printf("the value of z is %d \n", z);
+    return 0;
 }
